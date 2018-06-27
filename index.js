d456f8130583050d6dbf9350e188ebf699e54672
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 16:09:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,4 @@ function canBuildNote(magazine, note) {
 let magazine = ["h", "e", "r", "e", "a", "r", "e", "s", "o", "m",
 "e", "n", "i", "c", "e", "c", "l", "o", "t", "h", "e", "s", "t"];
 let note = "hand me the ferbie or else";
+console.log(canBuildNote(magazine,note));
