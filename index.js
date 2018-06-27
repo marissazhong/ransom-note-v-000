7c64711dde8100f73f32db164e929cf55cbf3e59
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 15:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-function buildHistogram(letters) {
+function buildHistogram(magazine) {
   magazineHistogram = {};
-  for (let i = 0; i < letters.length; i++) {
-    if (letters[i] in magazineHistogram) {
-      magazineHistogram[letters[i]] += 1;
+  for (let i = 0; i < magazine.length; i++) {
+    if (magazine[i] in magazineHistogram) {
+      magazineHistogram[magazine[i]] += 1;
     } else {
-      magazineHistogram[letters[i]] = 1;
+      magazineHistogram[magazine[i]] = 1;
     }
   }
   return magazineHistogram;
@@ -13,9 +13,9 @@ function buildHistogram(letters) {
 
 //console.log(buildHistogram(magazine));
 
-function binaryMatch(letters) {
+function binaryMatch(magazine, letter) {
   let letterCounts = buildHistogram(letters);
-  
+  // if 
 }
 
 let magazine = ["h", "e", "r", "e", "a", "r", "e", "s", "o", "m",
