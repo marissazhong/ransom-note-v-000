ee2c9090aff17165e2adaabe6f8431ac1a8ac09f
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 16:13:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ function canBuildNote(magazine, note) {
 
 let magazine = ["h", "e", "r", "e", "a", "r", "e", "s", "o", "m",
 "e", "n", "i", "c", "e", "c", "l", "o", "t", "h", "e", "s", "t"]
-let note = "handitover"
+let note = "iisat"
 console.log(canBuildNote(magazine,note));
