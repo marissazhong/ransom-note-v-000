1f66e4a293f9af710725caf27d6554e7139ae50d
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 15:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 function buildHistogram(letters) {
   magazineHistogram = {};
-  
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] in magazineHistogram) {
+      magazineHistogram[letters[i]] += 1;
+    } else {
+      magazineHistogram[letters[i]] = 1;
+    }
+  }
 }
 
 function binaryMatch() {
